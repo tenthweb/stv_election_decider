2ec1b2e004dee57449b158b101b6bedd5c7c78f9
tenthweb <tenthweb@gmail.com>
2025-12-18 13:01:54 +0000

    add worksheet-to-python-list converter for candidate names

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-'''the below code was taken with minor alterations from the Love Sandwiches project code'''
+'''the below code was taken with minor alterations from the 
+Love Sandwiches project code'''
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -14,10 +16,22 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Ballots')
+SHEET = GSPREAD_CLIENT.open('Election Data')
 
-ballots = SHEET.worksheet('Sheet1')
+candidate_worksheet = SHEET.worksheet('Candidates')
+ballots = SHEET.worksheet('Ballots')
+# ballots_raw = ballots.get_all_values()
 
-ballots_raw = ballots.get_all_values()
+'''We assume candidates have been given an ID by the ballot-setters and continue to use that one'''
 
-print(ballots)
+'''For test ballot sets, we could create a list of candidates and give them an ID by alphabetising them
+
+We don't want the candidate data to be changed, e.g. a user sneaking themselves onto the list!
+'''
+
+CANDIDATE_DATA =(candidate_worksheet.get_all_values())
+
+CANDIDATE_NAMES = [CANDIDATE_DATA[i][0] for i in range(len(CANDIDATE_DATA))]
+
+live_vote_table = {candidate for candidate in CANDIDATE_NAMES}
+print(CANDIDATE_NAMES)
