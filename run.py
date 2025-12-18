ffaf92ea47f678ee793d016b56ba3d46349b6629
tenthweb <tenthweb@gmail.com>
2025-12-18 14:40:50 +0000

    add ballot counting and droop quota functions

--- run.py
+++ run.py
@@ -20,6 +20,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Election Data')
 
+
+#Phase 1 (Data Input) Starts Here
+
 candidate_worksheet = SHEET.worksheet('Candidates')
 ballots = SHEET.worksheet('Ballots')
 # ballots_raw = ballots.get_all_values()
@@ -64,10 +67,42 @@ for line in BALLOTS_RAW:
         
 print(ballots_cleaned)
 
+'''The ballots_cleaned variable is a list of ballots. Each ballot is a list of 
+lists, representing candidate name and preference number.'''
+'''TODO make ballots into objects'''
+
 
 live_vote_count = {CANDIDATE_NAMES_AND_IDS[candidate]:0 for candidate in CANDIDATE_NAMES_AND_IDS}
 
 print("Current live_vote_count:")
 for value, key in live_vote_count.items():
     print(f"{value}: {key}")
+
+
+
+#Phase 2 (Counting) Starts Here
+
+'''Iterate through ballots, getting each ballot's first preference and adding one to that candidate's live vote count'''
+
+def get_individual_ballot_count():
+    ballot_count = len(ballots_cleaned)
+    return ballot_count
+
+print(f"Total number of ballots: {get_individual_ballot_count()}")
+
+def get_droop_quota(ballot_count):
+    droop_quota = (ballot_count // (3 + 1)) + 1
+    return droop_quota
+
+for ballot in ballots_cleaned:
+    for entry in ballot:
+        if entry[1] == '1':
+            candidate_name = entry[0]
+            live_vote_count[candidate_name] += 1
+
+print("Updated live_vote_count:")
+for value, key in live_vote_count.items():
+    print(f"{value}: {key}")
+
+
 
