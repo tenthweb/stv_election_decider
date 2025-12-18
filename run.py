50d054e57ad1fab744a799ec30966cdd8720076b
tenthweb <tenthweb@gmail.com>
2025-12-18 10:36:26 +0000

    add gspread to connect to ballots workbook, plus test code to check that it works

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+'''the below code was taken with minor alterations from the Love Sandwiches project code'''
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Ballots')
+
+ballots = SHEET.worksheet('Sheet1')
+
+ballots_raw = ballots.get_all_values()
+
+print(ballots)
